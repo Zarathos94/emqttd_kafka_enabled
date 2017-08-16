7754c31cbd0f5d9da8655779b58b1dab21e1dcd3
Zarathos94 <irfansaric94@gmail.com>
2017-08-16 18:21:40 +0200

    Changed start.sh environment passing

--- start.sh
+++ start.sh
@@ -182,10 +182,7 @@ echo $(echo "${EMQ_LOADED_PLUGINS}."|sed -e "s/^[^A-Za-z0-9_]\{1,\}//g"|sed -e "
 #RMQ_VHOST="/"
 #RMQ_PORT="5672"
 echo 'Loaded RMQ Config [HOST: '${RMQ__HOST}' | PORT: '${RMQ__PORT}' | USER: '${RMQ__USER}' | PASS : '${RMQ__PASS}']'
-RMQ__HOST=$(RMQ__HOST)
-RMQ__USER=$(RMQ__USER)
-RMQ__PORT=$(RMQ__PORT)
-RMQ__PASS=$(RMQ__PASS)
+
 echo "RMQ_HOST=${RMQ__HOST}"
 echo "RMQ_USER=${RMQ__USER}"
 echo "RMQ_PASS=${RMQ__PASS}"
